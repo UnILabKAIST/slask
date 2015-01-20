6fc4c0d9a1dcebbfb33755d1da3ee4d144d8e72c
arongdari <arongdari@gmail.com>
2015-01-20 17:16:28 +0900

    include 문지 campus menu

--- plugins/cafeteria_menu.py
+++ plugins/cafeteria_menu.py
@@ -37,12 +37,16 @@ def get_page(target_url, get_post = 1):
         return get_page(target_url, get_post)
 
 
-def eastmenu(target_menu_time):
-    east_cafeteria_menu_url_template = 'http://m.kaist.ac.kr/_prog/fodlst/index.php?site_dvs_cd=kr&menu_dvs_cd=050303&dvs_cd=fclt&dvs_cd=east1&stt_dt=%s-%s-%s&site_dvs=mobile'
+def menu(target_menu_time, loc):
+
+    cafeteria_menu_url_template = 'http://m.kaist.ac.kr/_prog/fodlst/index.php?site_dvs_cd=kr&menu_dvs_cd=050303&dvs_cd=fclt&dvs_cd=east1&stt_dt=%s-%s-%s&site_dvs=mobile'
+
+    if loc == '문지':
+        cafeteria_menu_url_template = 'http://m.kaist.ac.kr/_prog/fodlst/index.php?site_dvs_cd=kr&menu_dvs_cd=050303&dvs_cd=fclt&dvs_cd=icc&stt_dt=%s-%s-%s&site_dvs=mobile'
 
     today_date = datetime.datetime.now().strftime("%Y-%m-%d").split('-')
 
-    target_url = east_cafeteria_menu_url_template % (today_date[0], today_date[1], today_date[2])
+    target_url = cafeteria_menu_url_template % (today_date[0], today_date[1], today_date[2])
 
     target_page = get_page(target_url)
 
@@ -59,9 +63,9 @@ def eastmenu(target_menu_time):
     target_menu = target_menu.strip()
 
     # Send a message to #general channel
-    target_script = {1:'***오늘 동측 아침 메뉴***',
-    2: '***오늘 동측 점심 메뉴***',
-    3: '***오늘 동측 저녁 메뉴***'}[target_menu_time]
+    target_script = {1:'***오늘 %s 아침 메뉴***' % (loc),
+    2: '***오늘 %s 점심 메뉴***' % (loc),
+    3: '***오늘 %s 저녁 메뉴***' % (loc) }[target_menu_time]
 
     return target_script + "\n" + target_menu
 
@@ -69,22 +73,28 @@ def eastmenu(target_menu_time):
 def on_message(msg, server):
     text = msg.get("text", "")
     
-    match = re.findall(ur'\ub3d9\uce21\?', text, re.UNICODE)
+    east_match = re.findall(unicode('동측?'), text, re.UNICODE)
+    moonji_match = re.findall(unicode('문지?'), text, re.UNICODE)
+
     is_food_channel = msg.get("channel") == u'C030P72AH'
-    if not match or not is_food_channel:
+    if not (east_match or moonji_match) or not is_food_channel:
         return
 
     time_now = int(datetime.datetime.now().strftime("%H"))
 
+    loc = '동측'
+    if moonji_match:
+        loc = '문지'
+
     if time_now < 10:
         # morning
-        return eastmenu(1)
+        return menu(1, loc)
     elif time_now < 14:
         # lunch
-        return eastmenu(2)
+        return menu(2, loc)
     elif time_now < 19:
         # dinner
-        return eastmenu(3)
+        return menu(3, loc)
     else:
         return "내일 ㄱㄱ"
 
